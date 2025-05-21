2e2f793e03a4921204b8a308e9cab6f83ea7f8d3
D Osinga <46380170+osingaatje@users.noreply.github.com>
2025-05-21 16:46:45 +0200

    added neo-tree and treesitter configs

--- lua/douwe/lazy.lua
+++ lua/douwe/lazy.lua
@@ -26,6 +26,10 @@ require('lazy').setup {
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'NMAC427/guess-indent.nvim', -- Detect tabstop and shiftwidth automatically
 
+  require 'douwe.treesitter',
+
+  require 'douwe.neo-tree',
+
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
   -- keys can be used to configure plugin behavior/loading/etc.
@@ -716,7 +720,7 @@ require('lazy').setup {
   -- require 'kickstart.plugins.indent_line',
   -- require 'kickstart.plugins.lint',
   -- require 'kickstart.plugins.autopairs',
-  require 'kickstart.plugins.neo-tree',
+  -- require 'kickstart.plugins.neo-tree',
   -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
