8a6fba32540f9d6539006a17a2a208e303aa2e78
Douwe Osinga <douwe@oscom.nl>
2025-06-16 11:27:09 +0000

    show hidden files neotree

--- lua/douwe/neo-tree.lua
+++ lua/douwe/neo-tree.lua
@@ -1,6 +1,18 @@
 return {
   'nvim-neo-tree/neo-tree.nvim',
   branch = 'v3.x',
+  config = function()
+    require('neo-tree').setup {
+      filesystem = {
+        filtered_items = {
+          visible = true, -- This is what you want: If you set this to `true`, all "hide" just mean "dimmed out"
+          hide_dotfiles = false,
+          hide_gitignored = true,
+        }
+      }
+  }
+  end,
+
   dependencies = {
     'nvim-lua/plenary.nvim',
     'nvim-tree/nvim-web-devicons',
