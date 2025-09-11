2af50133569adcbeea482f94ed73123b38a3001c
Douwe Osinga <douwe@oscom.nl>
2025-09-11 09:56:35 +0000

    started with some Latex stuff - does it work? idk

--- lua/douwe/lazy.lua
+++ lua/douwe/lazy.lua
@@ -38,6 +38,8 @@ require('lazy').setup {
 
   require 'douwe.markdown',
 
+  require 'douwe.latex',
+
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
   -- keys can be used to configure plugin behavior/loading/etc.
