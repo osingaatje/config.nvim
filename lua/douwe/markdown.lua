1b0f7a047edff9f22f416921d18b81446720b86f
Douwe Osinga <douwe@oscom.nl>
2025-09-22 09:08:42 +0000

    turn off auto-open for Peek.nvim

--- lua/douwe/markdown.lua
+++ lua/douwe/markdown.lua
@@ -5,7 +5,7 @@ return {
     build = "deno task --quiet build:fast",
     config = function()
       require("peek").setup({
-        auto_load = true,        -- whether to automatically load preview when
+        auto_load = false,       -- whether to automatically load preview when
         close_on_bdelete = true, -- close preview window on buffer delete
 
         syntax = true,           -- enable syntax highlighting, affects performance
