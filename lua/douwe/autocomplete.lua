cb954407a9f2c30ad0b0fe2a01da45be966eeacd
Douwe Osinga <douwe@oscom.nl>
2025-10-08 17:02:53 +0200

    added autocomplete plugin (latex/code/...)

---
+++ lua/douwe/autocomplete.lua
@@ -0,0 +1,74 @@
+return {
+  'hrsh7th/nvim-cmp',
+  dependencies = {
+    -- Adds LSP completion capabilities
+    'hrsh7th/cmp-nvim-lsp',
+    'hrsh7th/cmp-path',
+    -- Adds a number of user-friendly snippets
+    'rafamadriz/friendly-snippets',
+    'saadparwaiz1/cmp_luasnip',
+    {
+      'L3MON4D3/LuaSnip',
+      build = (function()
+        -- Build Step is needed for regex support in snippets
+        -- This step is not supported in many windows environments
+        if vim.fn.has('win32') == 1 then
+          return
+        end
+        return 'make install_jsregexp'
+      end)(),
+      dependencies = {
+        'rafamadriz/friendly-snippets',
+      },
+    },
+  },
+  config = function()
+    local cmp = require('cmp')
+    local luasnip = require('luasnip')
+
+    -- Load friendly-snippets
+    require('luasnip.loaders.from_vscode').lazy_load()
+
+    -- mappings!
+    cmp.setup({
+      mapping = cmp.mapping.preset.insert({
+        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<C-e>'] = cmp.mapping.abort(),
+        ['<CR>'] = cmp.mapping.confirm({ select = true }),
+        ['<Tab>'] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item()
+          elseif luasnip.expand_or_jumpable() then
+            luasnip.expand_or_jump()
+          else
+            fallback()
+          end
+        end, { 'i', 's' }),
+        ['<S-Tab>'] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif luasnip.jumpable(-1) then
+            luasnip.jump(-1)
+          else
+            fallback()
+          end
+        end, { 'i', 's' }),
+      }),
+      snippet = {
+        expand = function(args)
+          luasnip.lsp_expand(args.body)
+        end,
+      },
+      completion = {
+        completeopt = 'menu,menuone,noinsert',
+      },
+      sources = cmp.config.sources({
+        { name = 'nvim_lsp' },
+        { name = 'luasnip' },
+        { name = 'path' },
+      }),
+    })
+  end,
+}
--- lua/douwe/lazy.lua
+++ lua/douwe/lazy.lua
@@ -40,6 +40,8 @@ require('lazy').setup {
 
   require 'douwe.latex',
 
+  require 'douwe.autocomplete',
+
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
   -- keys can be used to configure plugin behavior/loading/etc.
