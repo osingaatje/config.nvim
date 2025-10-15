df73bc82f61b6835d1357f631fce6a81b50b868b
Douwe Osinga <douwe@oscom.nl>
2025-10-15 14:58:52 +0200

    changed Right to Ctrl+Right for accepting suggestions

--- lua/douwe/autocomplete.lua
+++ lua/douwe/autocomplete.lua
@@ -32,13 +32,13 @@ return {
     -- mappings!
     cmp.setup({
       mapping = cmp.mapping.preset.insert({
-        ['<C-j>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-k>'] = cmp.mapping.scroll_docs(4),
+        --['<C-j>'] = cmp.mapping.scroll_docs(-4),
+        --['<C-k>'] = cmp.mapping.scroll_docs(4),
         ['<C-Space>'] = cmp.mapping.complete(),
         ['<C-e>'] = cmp.mapping.abort(),
         --    ['<CR>'] = cmp.mapping.confirm({ select = true }),
-        ['<Right>'] = cmp.mapping.confirm({ select = true }),
-        ['<Tab>'] = cmp.mapping(function(fallback)
+        ['<C-Right>'] = cmp.mapping.confirm({ select = true }),
+        ['<C-j>'] = cmp.mapping(function(fallback)
           if cmp.visible() then
             cmp.select_next_item()
             --   elseif luasnip.expand_or_jumpable() then
@@ -47,7 +47,7 @@ return {
             fallback()
           end
         end, { 'i', 's' }),
-        ['<S-Tab>'] = cmp.mapping(function(fallback)
+        ['<C-k>'] = cmp.mapping(function(fallback)
           if cmp.visible() then
             cmp.select_prev_item()
           elseif luasnip.jumpable(-1) then
